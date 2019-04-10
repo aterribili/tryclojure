02ff5d32c7efb84f8f3c35ad2896e038a9bbddb4
Abner Terribili <abner.terribili@gmail.com>
2019-04-10 15:23:23 -0300

    Update

--- project.clj
+++ project.clj
@@ -1,13 +1,14 @@
 (defproject tryclojure "0.1.0-SNAPSHOT"
   :description "A simple web-based Clojure REPL for trying out Clojure without having to install it."
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [lib-noir "0.8.1"]
-                 [compojure "1.1.6"]
-                 [ring-server "0.3.1"]
-                 [commons-lang/commons-lang "2.5"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [lib-noir "0.9.9"]
+                 [compojure "1.6.1"]
+                 [ring-server "0.5.0"]
+                 [commons-lang/commons-lang "2.6"]
                  [clojail "1.0.6"]]
   :jvm-opts ["-Djava.security.policy=example.policy" "-Xmx80M"]
   :min-lein-version "2.0.0"
   :uberjar-name "tryclojure-standalone.jar"
-  :plugins [[lein-ring "0.8.10"]]
+  :plugins [[lein-ring "0.8.10"]
+            [lein-ancient "0.6.15"]]
   :ring {:handler tryclojure.server/app :port 8801})
